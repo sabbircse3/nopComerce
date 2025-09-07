2f9f5e25dba77dd1ad3aea0cd0cc9889adfb96ba
sabbircse3 <sabbir.cse3@gmail.com>
2025-09-07 20:39:05 +0600

    news subscription successfully

--- tests/Newsletter.spec.js
+++ tests/Newsletter.spec.js
@@ -5,8 +5,21 @@ const { chromium } = require('playwright');
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  await page.goto('https://demo.nopcommerce.com/', { waitUntil: 'domcontentloaded' });
-  await page.getByRole('textbox', { name: 'Sign up for our newsletter' }).fill('sabbir.test@gmail.com');
-  await page.getByRole('button', { name: 'Subscribe' }).click();  
+  await page.goto('https://demo.nopcommerce.com/', { 
+  waitUntil: 'load',
+  timeout: 60000 // increase timeout to 60s
+});
+  await page.fill('#newsletter-email', 'sabbir5@example.com');
+await page.click('#newsletter-subscribe-button');
+
+await page.click('#newsletter-subscribe-button');
+
+await page.waitForFunction(() => {
+  const el = document.querySelector('#newsletter-result-block');
+  return el && el.textContent.trim().length > 0;
+}, { timeout: 15000 });
+
+const message = await page.locator('#newsletter-result-block').textContent();
+console.log("Newsletter Message:", JSON.stringify(message));
 
 })();
